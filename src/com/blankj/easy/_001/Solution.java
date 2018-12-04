006dc42f1a883e6938580fa992083d18a8cba16c
iacrfch@163.com <iacrfch@163.com>
2018-12-04 11:21:55 +0800

    twoSum方法增加注释

--- src/com/blankj/easy/_001/Solution.java
+++ src/com/blankj/easy/_001/Solution.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
  *     time  : 2017/04/21
  *     desc  :
  * </pre>
+ *
  */
 public class Solution {
 //    public int[] twoSum(int[] nums, int target) {
@@ -23,6 +24,16 @@ public class Solution {
 //        return null;
 //    }
 
+    /**
+     *判断数组内是否存在两个元素，相加的和等于目标数，如果存在，输出两个元素的下标
+     * 例：数组： [2, 7, 11, 15]
+     *    目标数： 9
+     *    期望输出结果：[0,1]
+     * @param nums
+     * @param target
+     * @return
+     *
+     */
     public int[] twoSum(int[] nums, int target) {
         int len = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
